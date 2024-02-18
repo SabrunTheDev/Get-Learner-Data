7dab946ec5713514a3fb603d986d299c1dd94bbc
Sabrun <officialsabrun@gmail.com>
2024-02-17 23:17:14 -0500

    Added script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript",
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+let getLearnerData = (CourseInfo, AssignmentGroup, LearnerSubmission) => {
+  let result = [];
+
+  for (let i = 0; i < LearnerSubmission.length; i++) {
+    submittedAssignment = {};
+    submittedAssignment["id"] = LearnerSubmission[i].learner_id;
+    let pointsPossible =
+      AssignmentGroup.assignments[LearnerSubmission[i].assignment_id - 1]
+        .points_possible;
+    submittedAssignment[`${LearnerSubmission[i].assignment_id}`] =
+      LearnerSubmission[i].submission.score / pointsPossible;
+
+    result.push(submittedAssignment);
+  }
+
+  return result;
+};
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+console.log(result);
